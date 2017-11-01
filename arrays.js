41c66db892677b98988196a15abb840e67808c3c
Amy Jackson <aj81623@gmail.com>
2017-11-01 19:12:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.pop(3);
+  return array.pop();
 }
 function removeElementFromEndOfArray(array){
   return array.slice(0, array.length -1);
